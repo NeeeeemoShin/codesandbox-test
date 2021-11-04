b3e74ccac4c04dbeea5b97227d45ce0f3fe2a678
NeeeeemoShin <92913474+NeeeeemoShin@users.noreply.github.com>
2021-11-04 13:42:45 +0900

    map追加

--- src/index.js
+++ src/index.js
@@ -155,23 +155,27 @@
 /**
  * mapやfilterを使った配列の処理
  */
-// const nameArr = ["田中", "山田", "じゃけえ"];
+
+//～番目は～です　としたいとき
+const nameArr = ["田中", "山田", "じゃけえ"];
 //従来の配列の中身を出力する記述
-// for (let i = 0; i < nameArr.length; i++) {
-//   console.log(nameArr[i]);
-// }
+for (let i = 0; i < nameArr.length; i++) {
+  console.log(`${i + 1}番目は${nameArr[i]}です`);
+}
+//上記も以下の1文で記述できる、mapの第二引数はindexが入る
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
 //以下がmapの基本的な構文
 // const nameArr2 = nameArr.map((name) => {
 //   return name;
 // })
 // console.log(nameArr2);
 //従来の記法がmapを使うことによって以下の1文で済むようになる
-// nameArr.map((name) => console.log(name));
 
 //ある条件になるもの(条件はfilterの中の関数で自分で指定する)を抽出できる
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 1;
-});
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
 
-console.log(newNumArr);
+// console.log(newNumArr);
