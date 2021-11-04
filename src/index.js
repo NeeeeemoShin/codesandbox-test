837c721600ab33ceb64d8e79caf93c9bb57037f3
NeeeeemoShin <92913474+NeeeeemoShin@users.noreply.github.com>
2021-11-04 11:59:05 +0900

    オブジェクトの分割代入

--- src/index.js
+++ src/index.js
@@ -70,17 +70,32 @@
 // function func1(str) {
 //  return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-// const func2 = (str) => str; この様にも書ける
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// // const func2 = (str) => str; この様にも書ける
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+const myProfile = {
+  name: "じゃけぇ",
+  age: 28
+};
+
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+console.log(message1);
+
+const { name, age } = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
